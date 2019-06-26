f4838c022488d1a76bcdc14034ebb707fd4ba652
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-06-26 11:56:23 -0500

    first steps

--- R/extract_data.R
+++ R/extract_data.R
@@ -2,22 +2,31 @@
 #'
 #' Creates an intermediate-form for the data.
 #'
+#' This returns a named list of dataset objects, with elements:
+#'
+#'
+#'
 #' @param data_plt A `list` of data from the ggplot object.
 #' @param layers_plt A `list` of layers from the ggplot object.
 #'
-#' @return `list` of named datasets
+#' @return named `list`, each of which has elements:
+#'  `metadata`, `variables`, `hash`
+#' @keywords internal
 #' @export
 #'
 #' @examples
 #' library(ggplot2)
 #' p <- ggplot(data = iris)
 #' p <- p + geom_point(aes(x = Petal.Width, y = Petal.Length))
-#' data_int(p$data, p$layers)
+#'
+#' data_int_demo <- data_int(p$data, p$layers)
+#'
 data_int <- function(data_plt, layers_plt) {
-  #join together default data and layer data
+
+  # join together default data and layer data
   data_all <- append(list(data_plt), purrr::map(layers_plt, purrr::pluck, "data"))
 
-  #format the lists of data
+  # format the lists of data
   data_all <- purrr::map(data_all, format_data_int)
 
   # how to name the datasets??
@@ -25,13 +34,8 @@ data_int <- function(data_plt, layers_plt) {
   # remove NULL entries
   data_all <- purrr::discard(data_all, is.null)
 
-
-
-
   data_all
-
 }
-
 
 #' Name the dataset
 #'
