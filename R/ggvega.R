51fee333a1769a6ef9b1f43d7ef445517b199dc8
Haley Jeppson <SESA495968@se.com>
2019-06-27 15:34:45 -0500

    adding ggspec
    still need to knit readme

--- R/ggvega.R
+++ R/ggvega.R
@@ -1,21 +1,41 @@
-#' Translate a ggspec
+#' Translate a ggspec object to a vlspec
 #'
-#' @param ggspec `list`, ggspec object
+#' @param ggspec `list`, a ggspec object
 #'
 #' @inherit vegawidget::as_vegaspec return
 #'
 #' @export
 #'
 ggvega <- function(ggspec) {
+
   ct <- V8::v8()
 
   ct$source(system.file("js", "ggvega.js", package = "ggvega"))
 
-  ct$assign("ggspec", jsonlite::toJSON(ggspec,auto_unbox = TRUE))
+  ct$assign("ggspec", jsonlite::toJSON(ggspec, auto_unbox = TRUE))
 
   ct$assign("ggspec",V8::JS("JSON.parse(ggspec)"))
 
   vlspec<-ct$get(V8::JS("ggvega.gg2vl(ggspec)"))
 
   vegawidget::as_vegaspec(vlspec)
+}
+
+
+#' Translate a ggplot2 object to a Vega-Lite spec
+#'
+#' @inheritParams vegawidget::as_vegaspec
+#'
+#' @inherit vegawidget::as_vegaspec return
+#'
+#' @export
+
+as_vegaspec.gg <- function(spec, ...) {
+
+  # convert  to ggspec
+  ggspec <- ggspec::gg2spec(spec)
+
+  # call ggvega
+  ggvega(ggspec)
+
 }
