8a35f7c61d419f24f8459c7746190841fdde5c35
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-06-27 19:54:22 -0500

    Add startup message

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "Welcome to ggvega; this package is not yet fully functional, and ",
+    "is under active development."
+  )
+}
