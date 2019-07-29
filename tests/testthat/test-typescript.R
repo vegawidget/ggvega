58e3579c1ba928727fa4a02b7df9da7fa4198849
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-07-28 21:39:27 -0500

    cleanup

--- tests/testthat/test-typescript.R
+++ tests/testthat/test-typescript.R
@@ -3,7 +3,6 @@ test_that("typescript tests work", {
   withr::with_dir(
     new = here::here("src-ext", "Typescript", "ggvega"),
     code = {
-      processx::run("yarn", "build")
       ts_results <- processx::run("yarn", "test", error_on_status = FALSE)
     }
   )
