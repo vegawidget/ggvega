947742d666023a3288e8adbe4add23a8bded0ecb
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-07-30 11:02:33 -0500

    Update with new deployment of examples

--- R/utils-example.R
+++ R/utils-example.R
@@ -2,24 +2,45 @@
 
 regex_ggplot_data <- function() {
 
+  # (                  - capture group
   # ggplot             - the word "ggplot"
   # \\s*               - optional whitespace
   # \\(\\s*            - open-parenthesis, optional whitespace
   # (?:data\\s*=\\s*)? - optional "data =" with whitespace, not captured
-  # ([\\w\\.]+)        - one-or-more word or dot characters, captured (what we want)
-  # .*                 - anything
+  # )
   #
-  "ggplot\\s*\\(\\s*(?:data\\s*=\\s*)?([\\w\\.]+).*"
+  # ([\\w\\.]+)        - one-or-more word or dot characters, captured (what we want)
+  #
+  # (.*)               - anything, capture
+  #
+  "(ggplot\\s*\\(\\s*)(?:data\\s*=\\s*)?([\\w\\.]+)(.*)"
 }
 
 regex_layer_data <- function() {
 
-  # \\w+\\s*           - function-name, optional whitespace
+  # (                  - capture group
+  # ^\\s*              - beginning-of-string, optional whitespace
+  # (?!ggplot)         - not the word "ggplot"
+  # .*                 - anything (will contain function name)
   # \\(.*              - open-parenthesis, anything
-  # (?:data\\s*=\\s*)  - "data =" with whitespace, not captured
+  # data\\s*=\\s*      - "data =" with whitespace
+  # )
+  #
   # ([\\w\\.]+)        - one-or-more word or dot characters, captured (what we want)
-  # .*                 - anything
-  "\\w+\\s*\\(.*(?:data\\s*=\\s*)([\\w\\.]+).*"
+  #
+  # (.*)               - anything, capture
+  #
+  "(^\\s*(?!ggplot).*\\(.*data\\s*=\\s*)([\\w\\.]+)(.*)"
+}
+
+head_data <- function(x, n = 1L){
+  # interpolate number of lines into replacement string
+  repl <- glue::glue("\\1head(\\2, {n})\\3")
+
+  x <- stringr::str_replace(x, regex_ggplot_data(), repl)
+  x <- stringr::str_replace(x, regex_layer_data(), repl)
+
+  x
 }
 
 
--- tests/testthat/test-utils-examples.R
+++ tests/testthat/test-utils-examples.R
@@ -1,7 +1,7 @@
 test_that("extraction of data name works for ggplot call", {
 
   extract <- function(x) {
-    stringr::str_replace(x, regex_ggplot_data(), "\\1")
+    stringr::str_replace(x, regex_ggplot_data(), "\\2")
   }
 
   expect_identical(extract("ggplot(iris)"), "iris")
@@ -20,12 +20,26 @@ test_that("extraction of data name works for ggplot call", {
 test_that("extraction of data name works for other calls", {
 
   extract <- function(x) {
-    stringr::str_replace(x, regex_layer_data(), "\\1")
+    stringr::str_replace(x, regex_layer_data(), "\\2")
   }
 
   expect_identical(extract("geom_point(data = iris)"), "iris")
   expect_identical(extract("geom_point(data=iris)"), "iris")
   expect_identical(extract("geom_point(data = iris, aes())"), "iris")
   expect_identical(extract("geom_point(aes(), data = iris"), "iris")
+
+  # if it's a ggplot call, do nothing
+  expect_identical(extract("ggplot(data = iris)"), "ggplot(data = iris)")
+
+})
+
+test_that("replacement function works", {
+
+  ex <- function(x, y) {
+    expect_identical(head_data(x), y)
+  }
+
+  ex("ggplot(iris) +", "ggplot(head(iris, 1)) +")
+  ex("geom_point(data = iris)", "geom_point(data = head(iris, 1))")
 
 })
