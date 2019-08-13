f5d6219330f14d66257b8bfecd058ea010168608
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-08-13 15:00:08 -0500

    Update documentation

--- dev/TypeScript/ggvega-signatures/src/itmEncodingObjectByStat.ts
+++ dev/TypeScript/ggvega-signatures/src/itmEncodingObjectByStat.ts
@@ -1,3 +1,27 @@
+/**
+ * Modify an encoding object according to a ggspec stat
+ * 
+ * @remark
+ * Note about side-effects.
+ * 
+ * This function is used to determine the specific function 
+ * according to the class of the ggspec stat. The actual work 
+ * is done in these specific functions.
+ * 
+ * Right now, we support only one ggspec stat: `StatIdentity`.
+ * 
+ * **Called by**
+ * @see itmLayer
+ * 
+ * **Calls**
+ * @see itmEncodingObjectByStatIdentity
+ * 
+ * @param itmEncodingObject 
+ * @param gsStat 
+ * @param gsStatParams 
+ * 
+ * @return itmEncodingObject
+ */
 function itmEncodingObjectByStat(
   itmEncodingObject: ItmEncodingObject, 
   gsStat: gs.Stat, 
@@ -19,7 +43,21 @@ function itmEncodingObjectByStat(
   return functionTranslate(itmEncodingObject, gsStat, gsStatParams);
 }
 
-
+/**
+ * Modify an encoding object according an identity stat
+ * 
+ * @remark
+ * This function does nothing.
+ * 
+ * **Called by**
+ * @see itmEncodingObjectByStat
+ * 
+ * @param itmEncodingObject 
+ * @param gsStat 
+ * @param gsStatParams 
+ * 
+ * @return itmEncodingObject
+ */
 function itmEncodingObjectByStatIdentity(
   itmEncodingObject: ItmEncodingObject, 
   gsStat: gs.Stat, 
