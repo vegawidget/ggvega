a6745c5390f16226290d46f4b94c6f5f898d5c44
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-08-13 13:49:15 -0500

    Update documentation

--- dev/TypeScript/ggvega-signatures/src/itmEncodingObject.ts
+++ dev/TypeScript/ggvega-signatures/src/itmEncodingObject.ts
@@ -3,18 +3,31 @@
  * 
  * @remark
  * This function creates an intermdiate `encoding` object using a 
- * ggspec `mapping` object and a ggspec `aesParams` object.
+ * ggspec `mapping` object and a ggspec `Metadata` object.
  * 
- * This could be split into two functions: one that deals with
- * `gsMappingObject` and `gsMetadataObject`
+ * Currently, a ggspec `mapping` is an object with a single key, `field`.
+ * Its value is a string; that string matches to a variable-name
+ * in the data.
+ * 
+ * In the near future, a ggspec `mapping` may instead contain a different
+ * single key, `stat`, with a string that matches an stat (aggregation) 
+ * operation, like `"count"`, or `"sum"`.
  * 
  * This is more of a note to myself, we are starting to see the patterns
  * of how ggplot2 mappings, aes_params, and stat default_aes are related.
  * I suspect this should be addressed in ggschema.
  * See <https://github.com/vegawidget/ggvega/issues/44>
  * 
+ * **Called by**
+ * 
+ * @see {@link itmLayer} to create an intermediate layer
+ * 
+ * **Calls**
+ * 
+ * @see {@link itmEncoding} to create an empty encoding
+ * @see {@link fieldName} to handle dots, ".", in field names
+ * 
  * @param gsMappingObject - `gs.Mapping` maps data varaibles to aesthetics
- * @param gsAesParamsObject - `gs.AesParams` maps values to aestheics
  * @param gsMetadataObject - `gs.Metadata` contains the metadata for the data
  *   associated to this layer
  * 
@@ -22,7 +35,6 @@
  */
 function itmEncodingObjectByMappingObject(
   gsMappingObject: gs.Mapping, 
-  gsAesParamsObject: gs.AesParams,
   gsMetadataObject: gs.Metadata
 ): ItmEncodingObject {
 
@@ -39,19 +51,23 @@ function itmEncodingObjectByMappingObject(
   // ggplot    - factor, ordered-factor, levels
   // Vega-Lite - nominal, ordinal, scale-domain
 
-  // for each member of gsMappingObject:
-  // - create an itmEncoding
-  // - populate the itmEncoding using the mapping
-  // - put itmEncoding into the itmEncodingObject 
-  for (let aesName in gsMappingObject) {
+  // for each mapping in gsMappingObject:
+  //   - extract the information from the mapping object, metadata
+  //   - create ItmEncoding
+  //   - populate ItmEncoding 
+  //   - put ItmEncoding into itmEncodingObject 
+  for (let aesName: string in gsMappingObject) {
     if (gsMappingObject.hasOwnProperty(aesName)) {
       // do we have a type/class for `mapping`?
 
       // extract information from mapping object, metatdata
       let mapping = gsMappingObject[aesName];
 
-      const field: string = fieldName(mapping.field);
-      const type: vl.StandardType = gsMetadata[field].type;
+      // TODO: we need to handle the situation where the mapping is a 
+      // `stat` instead of a `field`
+
+      let field: string = fieldName(mapping.field);
+      let type: vl.StandardType = gsMetadata[field].type;
 
       // create ItmEncoding
       let encoding: vl.Encoding = itmEncoding(aesName);
@@ -72,6 +88,49 @@ function itmEncodingObjectByMappingObject(
   return itmEncodingObject;
 }
 
+/**
+ * Modify an intermediate `encoding` object using an `AesParams` object
+ * 
+ * @remark
+ * This function will have side-effects, as the argument `itmEncodingObject`
+ * is mutable, and is modified inside this function. We return this object
+ * to signify that we intend to modify it, but we suspect that this is not
+ * strictly necessary; the function could return `null` to signify that it 
+ * is called for side-effects. I am curious to know the convention here.
+ * 
+ * This function modifies an intermdiate `encoding` object using a 
+ * ggspec `AesParams` object. In the future, we may need the `geom`.
+ * 
+ * A ggspec `AesParams` element is a key-value pair, where the key
+ * is a ggplot2 aesthetic-name, and the value can be a `string`, `number`,
+ * or `boolean` (?).
+ * 
+ * For each `AesParams` element, an `encoding` object is created, and put 
+ * into `itmEncodingObject` using the ggplot2 aesthetic-name as a key. The
+ * value (of this key-value pair) is an object with a single key-value
+ * pair: the key is `value`; its value is the value in "visual" space.
+ * 
+ * There are helper functions to translate values for shape, color, and size.
+ * 
+ * In the future, we may be able to take into account values that are specified 
+ * using "data" space: <https://github.com/vega/vega-lite/issues/1601>
+ * 
+ * **Called by**
+ * 
+ * @see {@link itmLayer} to create an intermediate layer
+ * 
+ * **Calls**
+ * 
+ * @see {@link itmEncoding} to create an empty encoding
+ * @see {@link encodingValueShape} to translate shape values
+ * @see {@link encodingValueColor} to translate color values
+ * @see {@link encodingValueSize} to translate size values
+ * 
+ * @param itmEncodingObject 
+ * @param gsAesParamsObject 
+ * 
+ * @return `ItmEncodingObject`
+ */
 function itmEncodingObjectByAesParamsObject(
   itmEncodingObject: ItmEncodingObject,
   gsAesParamsObject: gs.AesParams,
@@ -80,10 +139,11 @@ function itmEncodingObjectByAesParamsObject(
   // NOTE: we may have to pass the `geom` from itmLayer()
 
   // for each member of gsAesParams:
-  // - create an itmEncoding
-  // - populate the itmEncoding
-  // - put itmEncoding into the itmEncodingObject 
-  for (let aesName in gsAesParamsObject) {
+  //   - extract information from aesParams 
+  //   - create ItmEncoding
+  //   - populate ItmEncoding
+  //   - put ItmEncoding into itmEncodingObject 
+  for (let aesName: string in gsAesParamsObject) {
     if (gsAesParamsObject.hasOwnProperty(aesName)) {
 
       // extract information from aes_params
@@ -125,6 +185,7 @@ function itmEncodingObjectByAesParamsObject(
       itmEncodingObject[aesName] = encoding;
     }
   }
-    // return 
-    return itmEncodingObject;
+
+  // return 
+  return itmEncodingObject;
 }
