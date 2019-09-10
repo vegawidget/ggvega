dff6da6d2a2e5e6e173f731014818c603856ca67
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-09-10 17:32:19 -0500

    Add PositionStack and PositionFill to ggschema

--- src-ext/TypeScript/ggschema/src/layerPosition.ts
+++ src-ext/TypeScript/ggschema/src/layerPosition.ts
@@ -1,8 +1,19 @@
 export type Position = PositionIdentity;
 
 export interface PositionIdentity {
-
   position: {
     class: 'PositionIdentity';
+  }
+}
+
+export interface PositionStack {
+  position: {
+    class: 'PositionStack';
+  }
+}
+
+export interface PositionFill {
+  position: {
+    class: 'PositionFill';
   }
 }
