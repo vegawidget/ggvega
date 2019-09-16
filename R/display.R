529f4ec847725e003d5d15216c1007e8a6621198
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-09-15 23:04:10 -0500

    checking in for night

--- R/display.R
+++ R/display.R
@@ -1,20 +1,19 @@
-#' Create a markdown code-block from a ggplot2 example
+#' Display examples
 #'
-#' @inheritParams ggv_dev_path
+#' @param example `character` or `ggplot2` object, if `character`, uses
+#'   `ggv_example()` to retrieve a ggplot2 object.
 #' @param arrange `character`, arrangement of plots
 #' @param vl_width `integer`, width of VL chart (px.)
 #' @param vl_height `integer`, height of VL chart (px.)
 #' @param gg_height `integer`, width of GG chart (px.)
 #' @param gg_width `integer`, height of GG chart (px.)
 #'
-#' @return `glue::glue()` object
+#' @return Invisible `NULL`, called for side-effects
 #'
 #' @keywords internal
 #' @export
 #'
-ggv_dev_display <- function(example, arrange = c("side", "top"),
-                            vl_width = 275, vl_height = 275,
-                            gg_width = 400, gg_height = 320) {
+ggv_dev_display <- function(example, arrange = c("side", "top"), ...) {
 
 
   # note to include stacked arrangement of plots
@@ -37,49 +36,17 @@ ggv_dev_display <- function(example, arrange = c("side", "top"),
 
 
   # comparison
+  plt <- ggv_dev(example, "ggplot")
+  spec <- ggv_dev(example, "vegaspec")
 
   # knitr directory
   dir_files <-
     glue::glue("{tools::file_path_sans_ext(knitr::current_input())}_files")
 
-  file_gg <- fs::path_join(c(dir_files, glue::glue("{example}-gg.png")))
-  file_vl <- fs::path_join(c(dir_files, glue::glue("{example}-vl.svg")))
-
   fs::dir_create(dir_files)
+  file_root <- fs::path_join(c(dir_files, example))
 
-  suppressMessages(
-    ggplot2::ggsave(
-      file_gg,
-      plot = ggv_dev(example, "ggplot"),
-      device = "png",
-      width = 5,
-      height = 5 * gg_height / gg_width,
-      units = "in"
-    )
-  )
-
-  vl <- ggv_dev(example, "vegaspec")
-  vl$width <- vl_width
-  vl$height <- vl_height
-
-  vw_write_svg(vl, path = file_vl)
-
-  div_compare <-
-    tags$div(
-      tags$table(
-        tags$tr(
-          tags$td(
-            tags$img(src = file_gg, width = gg_width),
-            style = "border-width: 0px;"
-          ),
-          tags$td(
-            tags$img(src = file_vl),
-            style = "border-width: 0px;"
-          ),
-          style = "border-width: 0px;"
-        )
-      )
-    )
+  div_compare <- cmpre(plt, spec, file_root = file_root, use_svg = TRUE, ...)
 
   # JSON spec for ggspec & vegaspec
   ggspec_json <-
@@ -129,15 +96,25 @@ ggv_dev_display <- function(example, arrange = c("side", "top"),
       )
     )
 
-
   # print
-
   print(code_block)
   print(div_compare)
   print(div_json)
 
   invisible(NULL)
 }
+
+#' @rdname ggv_dev_display
+#'
+#' @keywords internal
+#' @export
+#'
+ggv_example_display <- function(example, arrange = c("side", "top"),
+                                vl_width = 275, vl_height = 275,
+                                gg_width = 400, gg_height = 320) {
+
+}
+
 
 as_codeblock <- function(text) {
 
@@ -164,4 +141,83 @@ remove_comments <- function(text) {
   text
 }
 
+cmpre <- function(plt, spec, file_root, arrange = c("side", "top"),
+                  use_svg = FALSE, gg_width = NULL, gg_height = NULL,
+                  vl_width = NULL, vl_height = NULL) {
 
+  arrange <- match.arg(arrange)
+
+  # dimensions
+  vl_width <- vl_width %||% 275
+  vl_height <- vl_height %||% 275
+
+  gg_width <- gg_width %||% 400
+  gg_height <- gg_height %||% 320
+
+  tags <- htmltools::tags
+
+  # filesystem
+  file_gg <- glue::glue("{file_root}-gg.png")
+  file_vl <- glue::glue("{file_root}-vl.svg")
+
+  suppressMessages(
+    ggplot2::ggsave(
+      file_gg,
+      plot = plt,
+      device = "png",
+      width = 5,
+      height = 5 * gg_height / gg_width,
+      units = "in"
+    )
+  )
+
+  spec$width <- vl_width
+  spec$height <- vl_height
+
+  if (use_svg) {
+    vw_write_svg(spec, path = file_vl)
+    tag_vl <- tags$img(src = file_vl)
+  } else {
+    tav_vl <- vegawidget(spec)
+  }
+
+  div_compare_side <-
+    tags$div(
+      tags$table(
+        tags$tr(
+          tags$td(
+            tags$img(src = file_gg, width = gg_width),
+            style = "border-width: 0px;"
+          ),
+          tags$td(
+            tag_vl,
+            style = "border-width: 0px;"
+          ),
+          style = "border-width: 0px;"
+        )
+      )
+    )
+
+  div_compare_top <-
+    tags$div(
+      tags$table(
+        tags$tr(
+          tags$td(
+            tags$img(src = file_gg, width = gg_width),
+            style = "border-width: 0px;"
+          )
+        ),
+        tags$tr(
+          tags$td(
+            tag_vl,
+            style = "border-width: 0px;"
+          ),
+          style = "border-width: 0px;"
+        )
+      )
+    )
+
+  list_div <- list(side = div_compare_side, top = div_compare_top)
+
+  list_div[[arrange]]
+}
